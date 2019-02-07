3f1a27a396287288710328f73e32ddbc6ba688de
alesiv <aner98lam@gmail.com>
2019-02-07 11:59:14 +0200

    Realization interface IList for CustomVector

--- CustomCollections/CustomCollections/Program.cs
+++ CustomCollections/CustomCollections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CustomCollections
 {
@@ -6,6 +7,25 @@ namespace CustomCollections
     {
         static void Main(string[] args)
         {
+            CustomVector<int> vector = new CustomVector<int>();
+            vector.Add(1);
+            vector.Add(3);
+            vector.Add(4);
+            vector.Add(5);
+            vector.Add(6);
+            var isElement = vector.Contains(3);
+            var remove = vector.Remove(3);
+            vector.Insert(1,2);
+            vector.RemoveAt(2);
+            var element = vector.IndexOf(0);
+            foreach (var item in vector)
+            {
+                Console.WriteLine(item);
+            }
+
+            var arr = new int[10];
+            vector.CopyTo(arr, 2);
+            vector.Clear();
         }
     }
 }
