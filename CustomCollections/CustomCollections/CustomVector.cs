2f00464dbdf45123bd56ea065f55cb23facf7333
alesiv <aner98lam@gmail.com>
2019-02-08 12:04:46 +0200

    Fix all problem in methods and add throw exseption when it's need

--- CustomCollections/CustomCollections/CustomVector.cs
+++ CustomCollections/CustomCollections/CustomVector.cs
@@ -9,14 +9,10 @@ namespace CustomCollections
     {
         private T[] _vector;
 
-        public int Count { get; private set; }
-        public bool IsReadOnly { get; private set; } = true;
+        public int Count { get; private set; } = 0;
+        public bool IsReadOnly { get; } = false;
 
-        public CustomVector()
-        {
-            _vector = new T[0];
-            Count = _vector.Length;
-        }
+        public CustomVector() => _vector = new T[0];
 
         public T this[int index]
         {
@@ -34,31 +30,54 @@ namespace CustomCollections
             newVector[Count] = item;
             Count++;
             _vector = newVector;
-            IsReadOnly = false;
         }
 
         public void Clear()
         {
             _vector = new T[0];
             Count = 0;
-            IsReadOnly = true;
         }
 
         public bool Contains(T item)
         {
-            if (_vector.Contains(item))
+            if (item.Equals(null))
             {
-                return true;
+                throw new ArgumentNullException();
+            }
+
+            for (int i = 0; i < Count; i++)
+            {
+                if (_vector[i].Equals(item))
+                {
+                    return true;
+                }
             }
             return false;
         }
 
         public bool Remove(T item)
         {
-            _vector = _vector.Where(s => !s.Equals(item)).ToArray();
-            if (Count > _vector.Length)
+            if (IsReadOnly)
             {
-                Count = _vector.Length;
+                throw new NotSupportedException();
+            }
+            if (item.Equals(null))
+            {
+                throw new ArgumentNullException();
+            }
+             
+            var index = -1;
+            for (int i = 0; i < Count; i++)
+            {
+                if (_vector[i].Equals(item))
+                {
+                    index = i;
+                    break;
+                }
+            }
+            if (!index.Equals(-1))
+            {
+                RemoveAt(index);
                 return true;
             }
             return false;
@@ -66,11 +85,31 @@ namespace CustomCollections
 
         public void RemoveAt(int index)
         {
-            Remove(_vector[index]);
+            if (index >= Count || index < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            var newVector = new T[Count - 1];
+            for (int i = 0; i < index; i++)
+            {
+                newVector[i] = _vector[i];
+            }
+            for (int i = index + 1; i < Count; i++)
+            {
+                newVector[i - 1] = _vector[i];
+            }
+            Count--;
+            _vector = newVector;
         }
 
         public void Insert(int index, T item)
         {
+            if (index < 0 || index >= Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
             Count++;
             var newVector = new T[Count];
             var j = 0;
@@ -91,6 +130,11 @@ namespace CustomCollections
 
         public int IndexOf(T item)
         {
+            if (item.Equals(null))
+            {
+                throw new ArgumentNullException();
+            }
+
             for (int i = 0; i < Count; i++)
             {
                 if (_vector[i].Equals(item))
@@ -98,17 +142,24 @@ namespace CustomCollections
                     return i;
                 }
             }
-
             return -1;
         }
 
         public void CopyTo(T[] array, int arrayIndex)
         {
+            if(array == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if(arrayIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
             if (array.Length - arrayIndex < Count)
             {
-                Console.WriteLine("You can't do this. The array is too small or arrayIndex is in the end of array");
-                return;
+                throw new ArgumentException();
             }
+
             for (int i = 0; i < Count; i++)
             {
                 array[arrayIndex] = _vector[i];
