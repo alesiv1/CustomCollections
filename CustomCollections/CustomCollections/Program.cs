baaf201913f2760727ec3719dca274afbf8f9062
alesiv <aner98lam@gmail.com>
2019-02-11 16:50:19 +0200

    Finish correct all problems. Last version 1.0

--- CustomCollections/CustomCollections/Program.cs
+++ CustomCollections/CustomCollections/Program.cs
@@ -7,17 +7,6 @@ namespace CustomCollections
     {
         static void Main(string[] args)
         {
-            var vector = new CustomVector<int>();
-            vector.Add(1);
-            vector.Add(2);
-             var a = vector.IndexOf(1);
-             var b = vector.IndexOf(11);
-
-             var vector2 = new CustomVector<string>();
-             vector2.Add("line1"); vector2.Add(null);
-             vector2.Add("line2");          
-             Console.WriteLine(vector2.Contains("line2"));
-             Console.WriteLine(vector2.Contains(null));
         }
     }
 }
